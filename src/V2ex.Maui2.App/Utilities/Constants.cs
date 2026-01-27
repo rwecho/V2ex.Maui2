d91f253ed54e66f897da3484f0c8c1149b1d9865
rwecho <rwecho@live.com>
2026-01-27 14:40:22 +0800

    feat: update Android UserAgent string for improved compatibility

--- src/V2ex.Maui2.App/Utilities/Constants.cs
+++ src/V2ex.Maui2.App/Utilities/Constants.cs
@@ -6,7 +6,7 @@ public static class UserAgentConstants
 #if WINDOWS
     public const string UserAgent = "Mozilla/5.0 (iPad; CPU OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1";
 #elif ANDROID
-    public const string UserAgent = "Mozilla/5.0 (Linux; Android 10; Tablet; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
+    public const string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15 Edg/130.0.0.0";
 #else
     public const string UserAgent = "Mozilla/5.0 (iPad; CPU OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1";
 #endif
